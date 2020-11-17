0270906750507bddfffba3a5b26c14b290209585
Shruti Sureshan <67042828+shruti-sureshan@users.noreply.github.com>
2020-11-18 00:04:05 +0530

    Add files via upload

---
+++ PHP/table of 5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<table align="left" border="1" cellpadding="3" cellspacing="0">
+<?php
+for($i=1;$i<=10;$i++)
+{
+echo "<tr>";
+$j=5;
+  echo "<td>$i * $j = ".$i*$j."</td>";
+ 
+  echo "<tr>";
+  }
+?>
+</table>
+</body>
+</html>
